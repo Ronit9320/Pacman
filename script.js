176330343708dec26728ab437a442522d4a79680
Ronit9320 <shahronit16@gmail.com>
2024-07-28 20:29:55 +0530

    initial design

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
-const canvas = document.getElementbyId('gameCanvas');
+const canvas = document.getElementById('gameCanvas');
 const context = canvas.getContext('2d');
 
 const tileSize = 20;
-const numRows = canvas.height / tileSize;
-const numCols = canvas.width / tileSize;
+const numRows = 30;
+const numCols = 30;
 
 const maze = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
-    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
-    [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
-];
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
+]
+
+let pellets = [];
 
 let pacMan = {
     x: 1,
@@ -35,15 +47,23 @@ let pacMan = {
     direction: 'right'
 };
 
+const ghosts = [
+    {x: 9, y: 8, direction: 'up', color: 'red'},
+    {x: 10, y: 8, direction: 'up', color: 'pink'},
+    {x: 9, y: 9, direction: 'up', color: 'cyan'},
+    {x: 10, y: 9, direction: 'up', color: 'orange'}
+]
+
+
 function drawTile(x, y, color){
     context.fillStyle = color;
-    context.fillrect(x * tileSize, y * tileSize, tileSize, tileSize);
+    context.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
 };
 
 function drawMaze(){
     for (let row = 0; row < numRows; row++){
         for (let col = 0; col < numCols; col++){
-            if (maze[row][col] === 1){
+            if (maze[row][col] == 1){
                 drawTile(col, row, 'blue');
             } else {
                 drawTile(col, row, 'black');
@@ -53,5 +73,117 @@ function drawMaze(){
 }
 
 function drawPacMan(){
-    
+    drawTile(pacMan.x, pacMan.y, 'yellow');
 }
+
+for (let row = 0; row < numRows; row++){
+    for (let col = 0; col < numCols; col++){
+        if (maze[row][col] === 0){
+            pellets.push({x: col, y: row});
+        }
+    }
+}
+
+function drawPellet(){
+    pellets.forEach(pellets => {
+        drawTile(pellets.x, pellets.y, 'white');
+    });
+}
+
+function drawGhosts(){
+    for (let ghost of ghosts){
+        drawTile(ghost.x, ghost.y, ghost.color);
+    }
+}
+
+function checkPelleteCollision(){
+    pellets = pellets.filter(pellet => !(pellet.x === pacMan.x && pellet.y === pacMan.y));
+}
+
+function updateGhosts(){
+    for (let ghost of ghosts){
+        let nextX = ghost.x;
+        let nextY = ghost.y;
+
+        if (Math.random() < 0.3){
+            const directions = ['up', 'down', 'left', 'right'];
+            ghost.direction = directions[Math.floor(Math.random() * directions.length)];
+        }
+
+        switch (ghost.direction){
+            case 'up':
+                nextY--;
+                break;
+
+            case 'down':
+                nextY++;
+                break;
+            case 'left':
+                nextX--;
+                break;
+            case 'right':
+                nextX++;
+                break; 
+        }
+        
+        if (nextX >= 0 && nextX < numCols && nextY >= 0 && nextY < numRows && maze[nextY][nextX] !== 1){
+            ghost.x = nextX;
+            ghost.y = nextY;
+        }
+    }
+}
+
+function updateGame(){
+    let nextX = pacMan.x;
+    let nextY = pacMan.y;
+
+    document.addEventListener('keydown', (event) => {
+        switch(event.key){
+            case 'ArrowUp':
+                pacMan.direction = 'up';
+                break;
+            case 'ArrowDown':
+                pacMan.direction = 'down';
+                break;
+            case 'ArrowLeft':
+                pacMan.direction = 'left';
+                break;
+            case 'ArrowRight':
+                pacMan.direction = 'right';
+                break;
+            }
+        });
+        
+        switch (pacMan.direction){
+            case 'up':
+                nextY--;
+                break;
+            case 'down':
+                nextY++;
+                break;
+            case 'left':
+                nextX--;
+                break;
+            case 'right':
+                nextX++;
+        }
+
+    if(maze[nextY][nextX] !== 1){
+        pacMan.x = nextX;
+        pacMan.y = nextY;
+    }
+    checkPelleteCollision();
+    updateGhosts();
+}
+
+function gameLoop(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    drawMaze();
+    drawPellet();
+    drawPacMan();
+    drawGhosts();
+    updateGame();
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
